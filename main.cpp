74e4ae6a8ac01b79a5522b38ffddf0184ef74265
Ibrahim Bello <ibrbello10@gmail.com>
2026-02-08 21:48:17 -0800

    Completed Milestone 3

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 struct Color {
@@ -19,6 +22,23 @@ int main(){
     colors.push_back(tempStruct); 
     cout << "Red: " << colors[0].red << ", Green: " << colors[0].green << 
     ", Blue: " << colors[0].blue << endl;
+
+    // Generate a random number between 25 and 50 for the number of colors to create
+    srand(time(0)); // Seed the random number generator
+    int numColors = rand() % 26 + 25;
+    // Create loop to generate random colors and store them in the vector
+    for (int i = 0; i < numColors; i++) {
+        Color randomColor;
+        randomColor.red = rand() % 256; 
+        randomColor.green = rand() % 256; 
+        randomColor.blue = rand() % 256; 
+        colors.push_back(randomColor); 
+    }
+    // Display the colors in the vector using range-based for loop
+    for (const Color& color : colors) {
+        cout << "Red: " << color.red << ", Green: " << color.green << 
+        ", Blue: " << color.blue << endl;
+    }
  
     return 0;
 }
